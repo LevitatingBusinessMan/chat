d1cda18f19ff7bd291e95ce5e7b962c3eee4e774
Rein F <public@reinfernhout.xyz>
2020-01-17 01:59:28 +0100

    messing around

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
 #define PORT 8080
+
+void *connectionHandler(int *, int *);
 
 int main() {
 	
@@ -45,15 +49,73 @@ int main() {
 
 	int conn_socket;
 	
-	char socket_reply[2000];
-
 	while (conn_socket = accept(socket_desc, (struct sockaddr *)&server, (socklen_t *)&addr_len)) {
 		
 		puts("Connection accepted");
 		
 		write(conn_socket , welcomeMessage , strlen(welcomeMessage));
 
+
+
+		pthread_t thread_id; 
+
+		struct arg_struct {
+			int* arg1;
+			int* arg2;
+		};
+
+		struct arg_struct args;
+
+		args.arg1 = malloc(1);
+		args.arg1 = conn_socket;
+
+		args.arg2 = activeConns;
+
+		if( pthread_create( &thread_id , NULL ,  connectionHandler , (void *)&args) < 0) {
+			puts("Could not create thread");
+			return 1;
+		}
+
+		pthread_join( thread_id , NULL);
+
 	}
+
+	return 0;
+
+}
+
+void *connectionHandler(int *conn_sock, int *activeConns) {
+	int sock = *(int*)conn_sock;
+
+	activeConns++;
+
+	printf("there are %d connections\n", &activeConns);
+
+	char *message;
+
+	message = "There are x connections\n";
+	write(sock, message, strlen(message));
+
+	int read_size;
+	char client_message[2000];
+
+	while( (read_size = recv(sock , client_message , 2000 , 0)) > 0)
+	{
+
+		//Send the message back to client
+		write(sock , client_message , strlen(client_message));
+
+	}
+
+	//Disconnected
+	if (read_size == 0) {
+		puts("Client disconnected");
+	} else {
+		puts("recv failed");
+	}
+
+	//Free the pointer (caused segmentation fault all of a sudden?)
+	//free(conn_sock);
 
 	return 0;
 
